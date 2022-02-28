e9dbc42baa173e130a64be900cae57bd5efda85b
Stevenlb <stevenpound@gmail.com>
2022-02-28 12:43:18 -0600

    Add files via upload

---
+++ Samples/PNP StoredCred.ps1
@@ -0,0 +1,8 @@
+﻿$RootSite = "https://nvashare-admin.sharepoint.com"
+$User = "Steven.Pound@nva.com"
+$Password = "Eviole.7"
+
+
+
+
+Add-PnPStoredCredential -Name $RootSite -Username $User -Password (ConvertTo-SecureString -String $Password -AsPlainText -Force)
