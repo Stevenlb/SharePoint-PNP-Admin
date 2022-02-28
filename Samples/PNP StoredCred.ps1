b7e661298aa70eb031bc50af3cb99a82c87ffb31
Stevenlb <stevenpound@gmail.com>
2022-02-28 12:44:22 -0600

    Delete PNP StoredCred.ps1

--- Samples/PNP StoredCred.ps1
+++
@@ -1,8 +0,0 @@
-﻿$RootSite = "https://nvashare-admin.sharepoint.com"
-$User = "Steven.Pound@nva.com"
-$Password = "Eviole.7"
-
-
-
-
-Add-PnPStoredCredential -Name $RootSite -Username $User -Password (ConvertTo-SecureString -String $Password -AsPlainText -Force)
